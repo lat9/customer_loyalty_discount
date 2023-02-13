eed16565cc988f9d658d71aa6c89c4b2f734cf66
lat9 <lat9@vinosdefrutastropicales.com>
2023-02-13 12:09:34 -0500

    #3, #4, #5: Various updates
    - Check configuration settings directly instead of using class variables.
    - Disallow loyalty-discount for orders placed by guests
    - Validate "Loyalty Discount Table", disabling use on storefront or messaging the admin if issues are found.
    - Removing various `float` conversions, in an initial attempt to 'lose' penny-off rounding errors.
    - Rename the `cutoff_date` function and variables, since the value's actually a `timestamp`.
    - Refactor the `install` method to make it more readable and provide more information regarding what the settings do.

--- includes/modules/order_total/ot_loyalty_discount.php
+++ includes/modules/order_total/ot_loyalty_discount.php
@@ -21,16 +21,11 @@ class ot_loyalty_discount
 
     protected
         $_check,
-        $include_shipping,
-        $include_tax,
-        $calculate_tax,
-        $table,
+        $discount_table,
         $loyalty_order_status,
-        $od_pc = 0,
-        $deduction,
+        $od_pc,
         $period_string,
-        $cum_order_total,
-        $cum_order_period;
+        $cum_order_total;
 
     public function __construct()
     {
@@ -43,14 +38,70 @@ class ot_loyalty_discount
             return false;
         }
 
-        $this->enabled = (MODULE_LOYALTY_DISCOUNT_STATUS === 'true'); 
-        $this->include_shipping = MODULE_LOYALTY_DISCOUNT_INC_SHIPPING;
-        $this->include_tax = MODULE_LOYALTY_DISCOUNT_INC_TAX;
-        $this->calculate_tax = MODULE_LOYALTY_DISCOUNT_CALC_TAX;
-        $this->table = MODULE_LOYALTY_DISCOUNT_TABLE;
+        $this->enabled = (MODULE_LOYALTY_DISCOUNT_STATUS === 'true');
+        if ($this->enabled === false) {
+            return false;
+        }
+
         $this->loyalty_order_status = MODULE_LOYALTY_DISCOUNT_ORDER_STATUS;
-        $this->cum_order_period = MODULE_LOYALTY_DISCOUNT_CUMORDER_PERIOD;
         $this->output = [];
+        $this->od_pc = 0;
+
+        // -----
+        // No loyalty-discount for guest-checkout purchases!
+        //
+        if (IS_ADMIN_FLAG === false && zen_in_guest_checkout() === true) {
+            $this->enabled = false;
+        } else {
+            $this->checkConfiguration();
+        }
+    }
+
+    // -----
+    // Check the order-total's configuration to see if the discount-table specification
+    // is 'processable'.
+    //
+    // The table's format is in the form 'vvv:ppp[,vvv:ppp]' where
+    //
+    // 1. Each 'vvv' represents the minimum value of the customer's previous order's amount
+    //    for which the 'ppp' percentage discount is to apply.  Each 'vvv' and 'ppp' value
+    //    is a numeric value, e.g. '5' or '5.00'.
+    // 2. Each 'vvv' value specified must be greater than the previous 'vvv' value.
+    //
+    protected function checkConfiguration()
+    {
+        $this->discount_table = [];
+
+        $discount_table = explode(',', str_replace(' ', '', MODULE_LOYALTY_DISCOUNT_TABLE));
+        $configuration_message = '';
+        if (empty($discount_table)) {
+            $configuration_message = 'No discounts configured.';
+        } else {
+            $last_discount_amount = 0;
+            foreach ($discount_table as $next_discount) {
+                if (strpos($next_discount, ':') === false) {
+                    $configuration_message = "Invalid discount-table format [$next_discount].";
+                    break;
+                }
+                list($discount_amount, $discount_percentage) = explode(':', $next_discount);
+                if (!is_numeric($discount_amount) || !is_numeric($discount_percentage)) {
+                    $configuration_message = "Invalid discount amount [$next_discount].";
+                    break;
+                }
+                if ($discount_amount <= $last_discount_amount) {
+                    $configuration_message = "Discount amounts must be in ascending order [$next_discount].";
+                    break;
+                }
+                $this->discount_table[$discount_amount] = $discount_percentage;
+                $last_discount_amount = $discount_amount;
+            }
+        }
+        if ($configuration_message !== '') {
+            $this->enabled = false;
+            if (IS_ADMIN_FLAG === true) {
+                $this->title .= '<small><span class="alert">(' . $configuration_message . ')</span></small>';
+            }
+        }
     }
 
     public function process()
@@ -63,8 +114,6 @@ class ot_loyalty_discount
 
         $od_amount = $this->calculate_credit($this->get_order_total(), $this->get_cum_order_total());
         if ($od_amount > 0) {
-            $this->deduction = $od_amount;
-
             $tmp = sprintf(MODULE_LOYALTY_DISCOUNT_INFO, $this->period_string, $currencies->format($this->cum_order_total), $this->od_pc . '%');
             $this->output[] = [
                 'title' => '<div class="ot_loyalty_title">' . $this->title . ':</div>' . $tmp,
@@ -84,12 +133,10 @@ class ot_loyalty_discount
 
         $od_amount = 0;
         $od_pc = false;
-        $table_cost_group = explode(',', MODULE_LOYALTY_DISCOUNT_TABLE);
-        foreach ($table_cost_group as $loyalty_group) {
-            $group_loyalty = explode(':', $loyalty_group);
-            if ($amount_cum_order >= $group_loyalty[0]) {
-                $od_pc = (float)$group_loyalty[1];
-                $this->od_pc = $od_pc;
+        foreach ($this->discount_table as $amount => $discount) {
+            if ($amount_cum_order >= $amount) {
+                $od_pc = $discount;
+                $this->od_pc = $discount;
             }
         }
 
@@ -101,20 +148,18 @@ class ot_loyalty_discount
         }
 
         // Calculate tax reduction if necessary
-        $od_amount = (round((float) $amount_order * 10) / 10) * ($od_pc / 100);
-        if ($this->calculate_tax === 'true') {
+        $od_amount = $amount_order * $od_pc / 100;
+        if (MODULE_LOYALTY_DISCOUNT_CALC_TAX === 'true') {
             // Calculate main tax reduction
-            $tod_amount = round($order->info['tax'] * 10) / 10;
-            $todx_amount = $tod_amount * ((float) $od_pc / 100);
+            $todx_amount = $order->info['tax'] * $od_pc / 100;
             $order->info['tax'] -= $todx_amount;
+            $od_amount += $todx_amount;
 
             // Calculate tax group deductions
-            $todx_amount = 0;
             foreach ($order->info['tax_groups'] as $key => $value) {
-                $god_amount = round($value * 10) / 10 * $od_pc / 100;
-                $order->info['tax_groups'][$key] -= $god_amount;
+                $order->info['tax_groups'][$key] -= ($value * $od_pc / 100);
             }
-            $od_amount += $todx_amount;
+
         }
 
         return $od_amount;
@@ -122,32 +167,31 @@ class ot_loyalty_discount
 
     public function get_order_total()
     {
-        global $order, $db;
+        global $order;
 
         $order_total = $order->info['total'];
-        $order_total_tax = $order->info['tax'];
-        // Check if gift voucher is in cart and adjust total
-        $products = $_SESSION['cart']->get_products();
-        for ($i = 0, $iMax = count($products); $i < $iMax; $i++) {
-            $t_prid = zen_get_prid($products[$i]['id']);
-            $gv_query = $db->Execute('select products_price, products_tax_class_id, products_model from ' . TABLE_PRODUCTS . " where products_id = '" . $t_prid . "'");            
-            if (preg_match('/^GIFT/', addslashes($gv_query->fields['products_model']))) {
-                $qty = $_SESSION['cart']->get_quantity($t_prid);
-                
-                $products_tax = zen_get_tax_rate($gv_result['products_tax_class_id']);
-                if ($this->include_tax == 'false') {
-                    $gv_amount = $gv_result['products_price'] * $qty;
-                } else {
-                    $gv_amount = ($gv_result['products_price'] + zen_calculate_tax($gv_result['products_price'], $products_tax)) * $qty;
-                }
-                $order_total = $order_total - $gv_amount;
+
+        // -----
+        // Gift certificates/vouchers aren't included in any discount, so their
+        // cost (and tax) will be removed from the order's total prior to return.
+        //
+        $gv_amount = 0;
+        $gv_tax = 0;
+        foreach ($order->products as $next_product) {
+            if (strpos($next_product['model'], 'GIFT') !== 0) {
+                continue;
             }
+
+            $current_gv_amount = $next_product['final_price'] * $next_product['qty'];
+
+            $gv_tax += zen_calculate_tax($current_gv_amount, $next_product['tax']);
+            $gv_amount += $current_gv_amount;
         }
-        $orderTotalFull = $order_total;
-        if ($this->include_tax === 'false') {
-            $order_total = $order_total - $order->info['tax'];
+
+        if (MODULE_LOYALTY_DISCOUNT_INC_TAX === 'false') {
+            $order_total = $order_total - $order->info['tax'] + $gv_tax;
         }
-        if ($this->include_shipping === 'false') {
+        if (MODULE_LOYALTY_DISCOUNT_INC_SHIPPING === 'false') {
             $order_total = $order_total - $order->info['shipping_cost'];
         }
         return $order_total;
@@ -168,55 +212,52 @@ class ot_loyalty_discount
                 AND o.orders_status >= " . $this->loyalty_order_status . "
               ORDER BY date_purchased DESC";
         $history_query = $db->Execute($history_query_raw);
-        $cum_order_total = 0;
+        $this->cum_order_total = 0;
         if (!$history_query->EOF) {
-            $cutoff_date = $this->get_cutoff_date();
+            $cutoff_timestamp = $this->get_cutoff_timestamp();
             foreach ($history_query as $next_order) {
-                if ($this->get_date_in_period($cutoff_date, $next_order['date_purchased']) === true) {
-                    $cum_order_total += $next_order['order_total'];
+                if ($this->get_date_in_period($cutoff_timestamp, $next_order['date_purchased']) === true) {
+                    $this->cum_order_total += $next_order['order_total'];
                 }
             }
         }
 
-        $this->cum_order_total = $cum_order_total;
-        return $cum_order_total;
+        return $this->cum_order_total;
     }
 
-    protected function get_cutoff_date()
+    protected function get_cutoff_timestamp()
     {
-        switch ($this->cum_order_period) {
+        switch (MODULE_LOYALTY_DISCOUNT_CUMORDER_PERIOD) {
             case 'year':
                 $this->period_string = MODULE_LOYALTY_DISCOUNT_YEAR;
-                $cutoff_date = strtotime('-1 year');
+                $cutoff_timestamp = strtotime('-1 year');
                 break;
 
             case 'quarter':
                 $this->period_string = MODULE_LOYALTY_DISCOUNT_QUARTER;
-                $cutoff_date = strtotime('-3 month');
+                $cutoff_timestamp = strtotime('-3 month');
                 break;
 
             case 'month':
                 $this->period_string = MODULE_LOYALTY_DISCOUNT_MONTH;
-                $cutoff_date = strtotime('-1 month');
+                $cutoff_timestamp = strtotime('-1 month');
                 break;
 
             case 'alltime':
             default:
                 $this->period_string = MODULE_LOYALTY_DISCOUNT_WITHUS;
-                $cutoff_date = 0;
+                $cutoff_timestamp = 0;
                 break;
         }
-        return $cutoff_date;
+        return $cutoff_timestamp;
     }
 
-    protected function get_date_in_period($cutoff_date, $raw_date)
+    protected function get_date_in_period($cutoff_timestamp, $raw_date)
     {
         if ($raw_date === '0000-00-00 00:00:00' || empty($raw_date)) {
             return false;
         }
-
-        $order_date_purchased = strtotime($raw_date);
-        return $order_date_purchased >= $cutoff_date;
+        return strtotime($raw_date) >= $cutoff_timestamp;
     }
 
     public function check()
@@ -244,7 +285,7 @@ class ot_loyalty_discount
             'MODULE_LOYALTY_DISCOUNT_INC_SHIPPING',
             'MODULE_LOYALTY_DISCOUNT_INC_TAX',
             'MODULE_LOYALTY_DISCOUNT_CALC_TAX',
-            'MODULE_LOYALTY_DISCOUNT_ORDER_STATUS'
+            'MODULE_LOYALTY_DISCOUNT_ORDER_STATUS',
         ];
     }
 
@@ -252,21 +293,26 @@ class ot_loyalty_discount
     {
         global $db;
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Display Total', 'MODULE_LOYALTY_DISCOUNT_STATUS', 'true', 'Do you want to enable the Order Discount?', 6, 1, 'zen_cfg_select_option([\'true\', \'false\'], ', now())");
+        $db->Execute(
+            "INSERT INTO " . TABLE_CONFIGURATION . "
+                (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added)
+             VALUES
+                ('Enable Discount?', 'MODULE_LOYALTY_DISCOUNT_STATUS', 'true', 'Do you want to enable the Loyalty Discount?', 6, 1, 'zen_cfg_select_option([\'true\', \'false\'], ', now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_LOYALTY_DISCOUNT_SORT_ORDER', '998', 'Sort order of display.', 6, 2, now())");
-        
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function ,date_added) values ('Include Shipping', 'MODULE_LOYALTY_DISCOUNT_INC_SHIPPING', 'true', 'Include Shipping in calculation', 6, 3, 'zen_cfg_select_option([\'true\', \'false\'], ', now())");
+                ('Sort Order', 'MODULE_LOYALTY_DISCOUNT_SORT_ORDER', '998', 'Sort order of display.', 6, 2, NULL, now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function ,date_added) values ('Include Tax', 'MODULE_LOYALTY_DISCOUNT_INC_TAX', 'true', 'Include Tax in calculation.', 6, 4, 'zen_cfg_select_option([\'true\', \'false\'], ', now())");
+                ('Include Shipping', 'MODULE_LOYALTY_DISCOUNT_INC_SHIPPING', 'true', 'Include Shipping in calculation', 6, 3, 'zen_cfg_select_option([\'true\', \'false\'], ', now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function ,date_added) values ('Calculate Tax', 'MODULE_LOYALTY_DISCOUNT_CALC_TAX', 'false', 'Re-calculate Tax on discounted amount.', 6, 5, 'zen_cfg_select_option([\'true\', \'false\'], ', now())");
+                ('Include Tax', 'MODULE_LOYALTY_DISCOUNT_INC_TAX', 'true', 'Include Tax in calculation.', 6, 4, 'zen_cfg_select_option([\'true\', \'false\'], ', now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function ,date_added) values ('Cumulative order total period', 'MODULE_LOYALTY_DISCOUNT_CUMORDER_PERIOD', 'year', 'Set the period over which to calculate cumulative order total.', 6, 6, 'zen_cfg_select_option([\'alltime\', \'year\', \'quarter\', \'month\'], ', now())");
+                ('Calculate Tax', 'MODULE_LOYALTY_DISCOUNT_CALC_TAX', 'false', 'Re-calculate Tax on discounted amount.', 6, 5, 'zen_cfg_select_option([\'true\', \'false\'], ', now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Discount Percentage', 'MODULE_LOYALTY_DISCOUNT_TABLE', '1000:5,1500:7.5,2000:10,3000:12.5,5000:15', 'Set the cumulative order total breaks per period set above, and discount percentages. <br><br>For example, in admin you have set the pre-defined rolling period to a month, and set up a table of discounts that gives 5.0% discount if they have spent over \$1000 in the previous month (i.e previous 31 days, not calendar month), or 7.5% if they have spent over \$1500 in the previous month.<br>', 6, 7, now())");
+                ('Cumulative Order Period', 'MODULE_LOYALTY_DISCOUNT_CUMORDER_PERIOD', 'year', 'Set the period over which to calculate the cumulative order total.', 6, 6, 'zen_cfg_select_option([\'alltime\', \'year\', \'quarter\', \'month\'], ', now()),
 
-        $db->Execute('INSERT INTO ' . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Order Status', 'MODULE_LOYALTY_DISCOUNT_ORDER_STATUS', '3', 'Set the minimum order status for an order to add it to the total amount ordered', 6, 8, now())");
+                ('Discount Percentage', 'MODULE_LOYALTY_DISCOUNT_TABLE', '1000:5,1500:7.5,2000:10', 'Set the cumulative order total breaks for the period set above and discount percentages.<br><br>The default value (<code>1000:5,1500:7.5,2000:10</code>) gives the customer:<ol><li>A 5% discount for a total &gt; 1000.</li><li>A 7.5% discount for a total &gt; 1500.</li><li>A 10% discount for a total &gt; 2000.</li></ol>', 6, 7, NULL, now()),
+
+                ('Order Status', 'MODULE_LOYALTY_DISCOUNT_ORDER_STATUS', '3', 'Set the minimum order status for an order to add it to the total amount ordered.', 6, 8, NULL, now())"
+        );
     }
 
     public function remove()
